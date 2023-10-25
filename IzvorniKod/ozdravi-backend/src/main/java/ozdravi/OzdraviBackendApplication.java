5a9fb3c48e8cfc61015e5862907205c8e9a64e97
Mia Vidakovic <mia.vidakovic@fer.hr>
2023-10-25 22:41:42 +0200

    add backend setup

---
+++ IzvorniKod/ozdravi-backend/src/main/java/ozdravi/OzdraviBackendApplication.java
@@ -0,0 +1,13 @@
+package ozdravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzdraviBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzdraviBackendApplication.class, args);
+	}
+
+}
