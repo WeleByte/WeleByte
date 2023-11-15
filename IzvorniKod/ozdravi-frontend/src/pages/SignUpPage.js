88e8a0fd5c14265cc36797b5b76804f678e662db
Krešimir Valečić <kresimir.valecic@fer.hr>
2023-11-15 11:10:20 +0100

    uspjesna impementacija logina

--- IzvorniKod/ozdravi-frontend/src/pages/SignUpPage.js
+++ IzvorniKod/ozdravi-frontend/src/pages/SignUpPage.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import loginVector from '../assets/images/loginVector.png';
 import { useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 const SignUpPage = () => {
 
-    const navigate = useNavigate();
-
-  const handleSignUp = (e) => {
+  const backendRoute = "http://localhost:8080"
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSignUp = async (e) => {
     e.preventDefault();
     // Perform login logic
+    const response = await fetch(backendRoute + "/register", {
+      method: 'POST',
+      headers: {
+        "Content-Type" : 'application/json'
+      },
+      body : JSON.stringify({
+        'username' : email,
+        'password' : password
+      })
+
+    })
+        // .then(res => res.json())
+        // .then(data => console.log(data))
+
+
     // If login is successful, navigate to the home page
-    console.log("hello")
-    navigate('/home');
+    if(!response.ok) {
+      throw new Error('Dogodila se greska')
+    } else {
+      navigate('/login');
+    }
+
+    console.log(email, password)
+
   };
 
   const navigateLogIn = () => {
@@ -33,14 +56,16 @@ const SignUpPage = () => {
           <form onSubmit={handleSignUp}> 
             <div className="mb-3">
               <label htmlFor="username" className="form-label" style={{float: 'left'}}>EMAIL</label>
-              <input type="text" className="form-control" id="username" />
+              <input type="text" className="form-control" id="username" value={email}
+                     onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div className="mb-3">
               <label htmlFor="password" className="form-label" style={{float: 'left'}}>ŠIFRA</label>
-              <input type="password" className="form-control" id="password" />
+              <input type="password" className="form-control" id="password" value={password}
+                onChange={(e) => setPassword(e.target.value)}/>
             </div>
             <button type="submit" className="btn btn-primary col-12 py-2">Registriraj se </button>
-            <p class = "pt-3" style={{fontSize: "13px"}}>Već ste član? <span style={{textDecoration: "underline ", cursor: "pointer"}} onClick={navigateLogIn}>Prijava</span> </p>
+            <p className = "pt-3" style={{fontSize: "13px"}}>Već ste član? <span style={{textDecoration: "underline ", cursor: "pointer"}} onClick={navigateLogIn}>Prijava</span> </p>
           </form>
         </div>
       </div>
