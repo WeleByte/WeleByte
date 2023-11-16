020ee9ef77d5ae5a250360abbfab720f430cfff6
Krešimir Valečić <kresimir.valecic@fer.hr>
2023-11-16 09:00:16 +0100

    Brisanje starih foldera

--- IzvorniKod/ozdravi-frontend/src/pages/Pedijatar/Pacijenti/KartonPacijenta/KartonPacijenta.js
+++
@@ -1,8 +0,0 @@
-// detailed view pacijenta
-
-// pristupiti povijesti medicinskih podataka
-// naruciti pacijenta na specijalisticki pregled/postupak
-// unositi podatke o pregledima
-
-
-// izlistati svu djecu ako si roditelj
--- IzvorniKod/ozdravi-frontend/src/pages/Pedijatar/Prijava.js
+++
@@ -1,1 +0,0 @@
-// prijaviti djecu
