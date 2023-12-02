858d33b1d6bd6489a5fc163d978a287d1d917859
Mihael <mihael.leskovic@fer.hr>
2023-12-02 14:01:10 +0100

    user svi atributi

--- IzvorniKod/ozdravi-backend/src/main/java/ozdravi/domain/User.java
+++ IzvorniKod/ozdravi-backend/src/main/java/ozdravi/domain/User.java
@@ -1,6 +1,8 @@
 package ozdravi.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
 /**
@@ -11,39 +13,42 @@ import jakarta.persistence.*;
 @Table(name = "users")
 public class User {
     @Id
+    @Getter @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter @Setter
     @Column(nullable = false, unique = true)
     private String username;
 
+    @Getter @Setter
     @Column(nullable = false)
     private String password;
 
+    @Getter @Setter
+    private Long parent_id;
+
+    @Getter @Setter
+    private Long doctor_id;
+
+    @Getter @Setter
+    private Long address_id;
+
+    @Getter @Setter
+    @Column(nullable = false, unique = true)
+    private Long oib;
+
+    @Getter @Setter
+    @Column(nullable = false, unique = true)
+    private Long first_name;
+
+    @Getter @Setter
+    @Column(nullable = false, unique = true)
+    private Long last_name;
+
+    @Getter @Setter
+    private String institution_email;
+
     public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long user_id) {
-        this.id = user_id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
