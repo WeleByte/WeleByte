430a7d8800da4980238c4c39dbf9addb7e5ec5b7
MiaVidakovic <147912721+MiaVidakovic@users.noreply.github.com>
2023-12-06 10:46:48 +0100

    Roles table (#50)
    * add Role and user_role entity
    
    * fill roles table

---
+++ IzvorniKod/ozdravi-backend/src/main/java/ozdravi/domain/Role.java
@@ -0,0 +1,14 @@
+package ozdravi.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
--- IzvorniKod/ozdravi-backend/src/main/resources/data.sql
+++ IzvorniKod/ozdravi-backend/src/main/resources/data.sql
@@ -1,2 +1,9 @@
 insert into users (username, password) values ('Marko', 'lozinka');
 insert into users (username, password) values ('Luka', 'pass');
+
+-- roles
+insert into roles (name) values ('admin');
+insert into roles (name) values ('child');
+insert into roles (name) values ('doctor');
+insert into roles (name) values ('parent');
+insert into roles (name) values ('pediatrician');
