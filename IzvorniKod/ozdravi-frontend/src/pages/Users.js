90074505e5f589d1af90e2cff3addf98b3353fe3
Leon Cvetkovski <57297698+LeonC8@users.noreply.github.com>
2023-12-09 18:13:29 +0100

    Add patient (#59)
    * nadogradnja registracije i logina
    
    provjeravanje unosa
    
    * nadogradnja registracije
    
    * register fix
    
    * napravio profile screen
    
    * fix
    
    * dodavanje-pacijenta
    
    * novi-pregled
    
    * m
    
    ---------
    
    Co-authored-by: Krešimir Valečić <kresimir.valecic@fer.hr>
    Co-authored-by: Dorian Matić <dorian.matic@fer.hr>

--- IzvorniKod/ozdravi-frontend/src/pages/Users.js
+++ IzvorniKod/ozdravi-frontend/src/pages/Users.js
@@ -1,22 +1,110 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import Navbar from '../components/Header';
 import userIcon from '../assets/images/userIcon.png'
 import AddPatient from '../components/AddPatient';
 import searchIcon from '../assets/icons/search.png'
 import chevronLeft from '../assets/icons/chevron-left.png'
 import chevronRight from '../assets/icons/chevron-right.png'
+import CloseIcon from '../assets/icons/x2.png'
+import TrashIcon from '../assets/icons/trash.png'
+import Plus2Icon from '../assets/icons/plus2.png'
+
+import NoviPregled from '../components/NoviPregled';
 
 
 const Users = () => {
 
   const uloga = "doktor"
   const [selectedUsers, setSelectedUsers] = useState('svi')
+  var currentOpenedOptions = null;
+  var optionsOpened= false;
 
   const [isAddPatientVisible, showAddPatient] = useState(false);
 
   const toggleAddPatient = () => {
     showAddPatient(!isAddPatientVisible);
+    
   };
+
+  const [noviPregledOtvoren, setNoviPregledOtvoren] = useState(false);
+
+  const toggleNoviPregled = () => {
+    setNoviPregledOtvoren(!noviPregledOtvoren);
+    closeUserOptions(currentOpenedOptions)
+  };
+
+  const closeUserOptions = (index) => {
+    console.log("closing")
+    for (var i=0; i <5; i++) {
+        const tbody = document.querySelector(`#usersTable tbody`);
+        const tr = tbody.querySelector(`#usersTable tr:nth-child(${i + 1})`);
+       
+        
+            const userOptions = tr.querySelector('.userOptions');
+           
+              userOptions.style.display = 'none';
+    }
+   
+        
+    
+  }
+
+  const openUserOptions = (index) => {
+    console.log("opening")
+    for (var i=0; i <5; i++) {
+        closeUserOptions(i);
+    }
+
+    optionsOpened = true;
+
+    const tbody = document.querySelector(`#usersTable tbody`);
+    const tr = tbody.querySelector(`#usersTable tr:nth-child(${index + 1})`);
+   
+    
+        const userOptions = tr.querySelector('.userOptions');
+      
+          userOptions.style.display = 'block';
+        
+    currentOpenedOptions = index;
+  }
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      // Handle the click event here
+      console.log('Component clicked!', event);
+      const tbody = document.querySelector(`#usersTable tbody`);
+    const tr = tbody.querySelector(`#usersTable tr:nth-child(${currentOpenedOptions + 1})`);
+    
+    const userOptions = tr.querySelector('.userOptions');
+   
+   
+    if (!(userOptions.contains(event.target)) && optionsOpened) {
+       
+
+        
+            if (userOptions.style.display !== "none") {
+                console.log("closing")
+                /* closeUserOptions(currentOpenedOptions);
+                optionsOpened = false */;
+            }
+            
+        
+    }
+
+    };
+
+    // Add click event listener when the component mounts
+    document.addEventListener('click', handleClick);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
+
+
+
 
   const original = [
       {
@@ -51,9 +139,9 @@ const Users = () => {
   }
 
 
-  const finalUsersList = filteredUsers.map((user) =>
+  const finalUsersList = filteredUsers.map((user, index) =>
       (
-          <tr>
+          <tr  style={{ position: 'relative' }}>
               <td scope="row">
                   <img src = {userIcon} alt = "" width = "14" className='me-3' style={{opacity: "75%"}}></img>
                   {user.ime + " " + user.prezime}
@@ -63,7 +151,19 @@ const Users = () => {
               <td>{user.visitCount}</td>
               <td>{user.email}</td>
 
-              <td class = "three-dot-td"><img width="18" height="18" src="https://img.icons8.com/ios-glyphs/30/menu-2.png" alt="menu-2"/></td>
+              <td class = "three-dot-td" >
+              
+
+                <img width="18" height="18" onClick={() => openUserOptions(index)}  src="https://img.icons8.com/ios-glyphs/30/menu-2.png" alt="menu-2"/></td>
+
+                <ul class="list-group userOptions shadow-lg p-0 border" style={{display:"none"}}>
+                <p class ="mb-2 mt-2 ps-3 py-1" style={{textAlign: "left"}}>Akcije <img class =" mt-1 closeActionsIcon" style={{ height: "19px", float: "right", opacity: "80%"}} onClick={() => closeUserOptions(index)} src={CloseIcon}></img>  </p>
+                <hr class ="mt-0 mb-0" style={{opacity: "20%"}}></hr>
+                <button onClick={toggleNoviPregled} class =" ps-3 col-12 mb-2 mt-2 py-2 novi-pregled-btn" style={{opaciy: "80%",textAlign: "left", fontWeight:"500", border:"none", background:"none"}} > Novi pregled  <img class ="me-3 mt-1" style={{ height: "19px", float: "right", opacity: "80%" }} src={Plus2Icon}></img> </button>
+                
+                <button class =" ps-3  col-12 mb-2 py-2 delete-btn" style={{opaciy: "80%",textAlign: "left", fontWeight:"500", border:"none", background:"none"}}> Izbriši <img class ="me-3 mt-1" style={{ height: "19px", float: "right", opacity: "800%" }}  src={TrashIcon}></img> </button>
+                
+                </ul>
 
           </tr>
       ))
@@ -77,6 +177,7 @@ const Users = () => {
      <Navbar></Navbar>
 
      {isAddPatientVisible && <AddPatient closeAddPatient = {toggleAddPatient}/>}
+     {noviPregledOtvoren && <NoviPregled closeNoviPregled = {toggleNoviPregled}/>}
      
 
 
@@ -84,7 +185,7 @@ const Users = () => {
    
 
     <h3 className = "pt-3 px-4 mt-2 " style={{textAlign: "left", maxWidth: "1246px"}}>Pacijenti {/* <button className='btn btn-tertiary mt-1' style={{float: 'right'}}>Povijest </button>  */} <button class = "btn btn-primary" style={{float:"right"}} onClick= {toggleAddPatient}>Novi Pacijent +</button> </h3>
-<p style={{textAlign: "left", maxWidth: "1200px"}} className = "px-4 mb-2 ">{odrasliCount} odraslih • {djecaCount} djece</p>
+{/* <p style={{textAlign: "left", maxWidth: "1200px"}} className = "px-4 mb-2 ">{odrasliCount + djecaCount} pacijenata</p> */}
 
 
 
@@ -118,7 +219,7 @@ const Users = () => {
     
 
     <div className='px-4 pt-0'>
-     <table class="table table-hover  table-bordered " id= "usersTable" style={{maxWidth: "1200px"}}>
+     <table class="table  table-bordered " id= "usersTable" style={{maxWidth: "1200px"}}>
     
 
 
