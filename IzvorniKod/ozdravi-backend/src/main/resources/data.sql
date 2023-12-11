b698bd16b145c854ef8a40bcbf3a3987c68486eb
Dorian Matić <dorian.matic@fer.hr>
2023-12-11 19:00:23 +0100

    add ADMIN role to prvi@mail.com

--- IzvorniKod/ozdravi-backend/src/main/resources/data.sql
+++ IzvorniKod/ozdravi-backend/src/main/resources/data.sql
@@ -1,17 +1,6 @@
-insert into users (username, password, oib, first_name, last_name)
-values ('prvi@mail.com',
-        '{bcrypt}$2a$12$vyhf.Rod8fQmYsXi7fooHOGIzq5gLXMZ5kQtzywRCiRrgCe41o9lq',
-        '69435151530',
-        'Ivan',
-        'Horvat');
+-- noinspection SqlNoDataSourceInspectionForFile
 
--- prvi korisnik ima parent, doctor i address id te institution email ==NULL
--- to se moze lako promijeniti dodavanjem tih polja u prvu zagradu i njihovih vrijednosti u drugu zagradu
--- bitno je paziti da je isti poredak i gore i dolje
--- nije bitno paziti da je isti poredak kao i u User klasi, tj. kao i u bazi
--- pass je 'primus" TODO obrisati komentar u nekom trenutku
-
--------------------------
+-- noinspection SqlDialectInspectionForFile
 
 -- roles
 insert into roles (name) values ('admin');
@@ -19,3 +8,19 @@ insert into roles (name) values ('child');
 insert into roles (name) values ('doctor');
 insert into roles (name) values ('parent');
 insert into roles (name) values ('pediatrician');
+
+-- default admin user
+insert into users (username, password, oib, first_name, last_name)
+values ('prvi@mail.com',
+        '{bcrypt}$2a$12$vyhf.Rod8fQmYsXi7fooHOGIzq5gLXMZ5kQtzywRCiRrgCe41o9lq',
+        '69435151530',
+        'Ivan',
+        'Horvat');
+
+insert into user_roles (user_id, role_id) values (1, 1);
+
+-- prvi korisnik ima parent, doctor i address id te institution email ==NULL
+-- to se moze lako promijeniti dodavanjem tih polja u prvu zagradu i njihovih vrijednosti u drugu zagradu
+-- bitno je paziti da je isti poredak i gore i dolje
+-- nije bitno paziti da je isti poredak kao i u User klasi, tj. kao i u bazi
+-- pass je 'primus" TODO obrisati komentar u nekom trenutku
