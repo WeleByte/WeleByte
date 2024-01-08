004714178a90060c2ee87b08717beceb9f6d530e
Mia Vidakovic <mia.vidakovic@fer.hr>
2024-01-08 19:28:30 +0100

    small fix

--- IzvorniKod/ozdravi-backend/src/main/java/ozdravi/rest/controllers/InstructionController.java
+++ IzvorniKod/ozdravi-backend/src/main/java/ozdravi/rest/controllers/InstructionController.java
@@ -3,6 +3,7 @@ package ozdravi.rest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ozdravi.domain.Instruction;
