980a1748944f4cdba3c6dd5d17e1f3f0e03d8640
Dorian Matić <dorian.matic@fer.hr>
2024-01-14 11:31:07 +0100

    enable parents to only update institution_email parameter on Users, as defined in UCs

--- IzvorniKod/ozdravi-frontend/src/pages/Profile.js
+++ IzvorniKod/ozdravi-frontend/src/pages/Profile.js
@@ -1,181 +1,187 @@
 import React, {useEffect} from 'react';
 import loginVector from '../assets/images/loginVector.png';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useState} from 'react';
 import Navbar from '../components/Header';
 
 const ProfilePage = (props) => {
-  const backendRoute = props.backendRoute
-  const navigate = useNavigate();
-  const [user, setUser] = useState('')
-  const [firstName, setFirstName] = useState(user.first_name)
-  const [lastName, setLastName] = useState(user.last_name)
-  const [OIB, setOIB] = useState(user.oib)
-  const [institutionEmail, setInstitutionEmail] = useState(user.institution_email)
-  const [incorrectName, setIncorrectName] = useState(false)
-  const [incorrectOIB, setIncorrectOIB] = useState(false)
-  const [incorrectEmailFormat, setIncorrectEmailFormat] = useState(false)
-  const [saveFailed, setSaveFailed] = useState(false)
-  const bearerToken = sessionStorage.bearerToken
-  const [errorMessage, setErrorMessage] = useState('')
+    const backendRoute = props.backendRoute
+    const navigate = useNavigate();
+    const [user, setUser] = useState('')
+    const [firstName, setFirstName] = useState(user.first_name)
+    const [lastName, setLastName] = useState(user.last_name)
+    const [OIB, setOIB] = useState(user.oib)
+    const [institutionEmail, setInstitutionEmail] = useState(user.institution_email)
+    const [incorrectName, setIncorrectName] = useState(false)
+    const [incorrectOIB, setIncorrectOIB] = useState(false)
+    const [incorrectEmailFormat, setIncorrectEmailFormat] = useState(false)
+    const [saveFailed, setSaveFailed] = useState(false)
+    const bearerToken = sessionStorage.bearerToken
+    const [errorMessage, setErrorMessage] = useState('')
 
-  useEffect(() => {
-    if(bearerToken === '' || bearerToken === null || bearerToken === undefined) {
-      navigate('/login')
-    } else {
-      const fetchedUser = (JSON.parse(sessionStorage.userData))
-      setUser(fetchedUser)
-      setFirstName(fetchedUser.first_name)
-      setLastName(fetchedUser.last_name)
-      setOIB(fetchedUser.oib)
-      setInstitutionEmail(fetchedUser.institution_email)
+    useEffect(() => {
+        if (bearerToken === '' || bearerToken === null || bearerToken === undefined) {
+            navigate('/login')
+        } else {
+            const fetchedUser = (JSON.parse(sessionStorage.userData))
+            setUser(fetchedUser)
+            setFirstName(fetchedUser.first_name)
+            setLastName(fetchedUser.last_name)
+            setOIB(fetchedUser.oib)
+            setInstitutionEmail(fetchedUser.institution_email)
+        }
+    }, []);
+    const handleSignOut = () => {
+        sessionStorage.clear()
+        localStorage.clear()
+        navigate('/login')
     }
-  }, []);
-  const handleSignOut = () => {
-    sessionStorage.clear()
-    localStorage.clear()
-    navigate('/login')
-}
 
-  const handleSave = async (e) => {
-    e.preventDefault();
-    //check login format
-    if((firstName !== '' || lastName !== '') && (firstName.length < 2 || lastName.length < 2)){
-      setIncorrectName(true)
-      return
+    const handleSave = async (e) => {
+        e.preventDefault();
+
+        if (!(institutionEmail === '' || institutionEmail === undefined || institutionEmail === null)
+            && !(/\S+@\S+\.\S+/.test(institutionEmail))) {
+            setIncorrectEmailFormat(true)
+            return
+        }
+        setIncorrectEmailFormat(false)
+
+        //if format is correct, check which fields are empty
+        // If login is successful, navigate to the home page
+
+        const response = await fetch(backendRoute + `/user/${user.id}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${bearerToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'institution_email': institutionEmail,
+            })
+        })
+
+        if (!response.ok) {
+            setSaveFailed(true)
+            setErrorMessage("Dogodila se pogreska")
+        } else {
+            setSaveFailed(false)
+            user.institution_email = institutionEmail
+            sessionStorage.setItem('userData', JSON.stringify(user))
+
+            navigate('/home');
+        }
+    };
+
+    if (!bearerToken) {
+        return null
     }
-    setIncorrectName(false)
+    return (
+        <div id="HomePageWrapper">
+            <Navbar></Navbar>
+            <div className="row" id="loginRow" style={{paddingTop: "50px"}}>
 
-    if(OIB !== '' && OIB.length !== 11) {
-      setIncorrectOIB(true)
-      return
-    }
-    setIncorrectOIB(false)
+                <div className="col-12 mx-auto ">
+                    <h2 className="text-start">Vaš profil</h2>
+                    <form onSubmit={handleSave} className="pt-3">
+                        <div style={{display: "grid", gridTemplateColumns: "1fr 1fr", gridColumnGap: "20px"}}>
 
-    if(!(institutionEmail === '' || institutionEmail === undefined || institutionEmail === null)
-        && !(/\S+@\S+\.\S+/.test(institutionEmail))) {
-      setIncorrectEmailFormat(true)
-      return
-    }
-    setIncorrectEmailFormat(false)
+                            {/*----------------------------FIRST NAME-----------------------------*/}
+                            <div className="mb-3">
+                                <label htmlFor="username" className="form-label" style={{float: 'left'}}>IME</label>
+                                <input type="text" className="form-control"
+                                       id="username" value={user.first_name}
+                                       disabled={true}
+                                       onChange={(e) =>
+                                           setFirstName(e.target.value.replace(/[^a-zA-ZščćžđöüäŠČĆŽĐÖÜÄ\\s]/, ''))}/>
+                            </div>
 
-    //if format is correct, check which fields are empty
-    if(firstName === '') setFirstName(user.first_name)
-    if(lastName === '') setLastName(user.last_name)
-    if(OIB === '') setOIB(user.oib)
-    if(institutionEmail === '') setInstitutionEmail(user.institution_email)
+                            {/*----------------------------LAST NAME-----------------------------*/}
+                            <div className="mb-3">
+                                <label htmlFor="username" className="form-label" style={{float: 'left'}}>PREZIME</label>
+                                <input type="text" className="form-control"
+                                       disabled={true}
+                                       id="username" value={user.last_name}
+                                       onChange={(e) =>
+                                           setLastName(e.target.value.replace(/[^a-zA-ZščćžđöüäŠČĆŽĐÖÜÄ\\s]/, ''))}/>
+                            </div>
+
+                            {/*-------------------------------OIB--------------------------------*/}
+                            <div className="mb-3">
+                                <label htmlFor="username" className="form-label" style={{float: 'left'}}>OIB</label>
+                                <input type="text" className="form-control"
+                                       disabled={true}
+                                       id="username" value={user.oib}
+                                       onChange={(e) =>
+                                           setOIB(e.target.value.replace(/\D/g, ''))}/>
+                            </div>
+
+                            {/*------------------------ EMAIL-------------------------*/}
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label" style={{float: 'left'}}>E-MAIL</label>
+                                <input type="text" className="form-control"
+                                       disabled={true}
+                                       id="username" value={user.email}
+                                       onChange={(e) => setInstitutionEmail(e.target.value)}/>
+                            </div>
 
 
-    //data verification
-    // If login is successful, navigate to the home page
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label" style={{float: 'left'}}>ULICA</label>
+                                <input type="text" className="form-control"
+                                       disabled={true}
+                                       id="username" value={user.address?.street}
+                                       onChange={(e) => setInstitutionEmail(e.target.value)}/>
+                            </div>
 
-    const response = await fetch(backendRoute + `/user/${user.id}`, {
-      method: 'PUT',
-      headers: {
-        'Authorization': `Bearer ${bearerToken}`,
-        'Content-Type' : 'application/json'
-      },
-      body : JSON.stringify({
-        'email': user.email,
-        'password': user.password,
-        'institution_email': institutionEmail,
-        'oib': OIB,
-        'first_name': firstName,
-        'last_name': lastName
-      })
-    })
 
-    // if(response.status)
-    if(response.status === 400){
-      setErrorMessage("OIB je neispravan")
-      setSaveFailed(true)
-    } else if(response.status === 401){
-      handleSignOut()
-    } else if(!response.ok) {
-      setSaveFailed(true)
-      setErrorMessage("Dogodila se pogreska")
-    } else {
-      setSaveFailed(false)
-      user.first_name = firstName
-      user.last_name = lastName
-      user.oib = OIB
-      user.institution_email = institutionEmail
-      sessionStorage.setItem('userData', JSON.stringify(user))
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label" style={{float: 'left'}}>KUĆNI
+                                    BROJ</label>
+                                <input type="text" className="form-control"
+                                       disabled={true}
+                                       id="username" value={user.address?.number}
+                                       onChange={(e) => setInstitutionEmail(e.target.value)}/>
+                            </div>
 
-      navigate('/home');
-    }
-  };
 
-  if(!bearerToken){
-    return null
-  }
-  return (
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label" style={{float: 'left'}}>GRAD</label>
+                                <input type="text" className="form-control"
+                                       disabled={true}
+                                       id="username" value={user.address?.city}
+                                       onChange={(e) => setInstitutionEmail(e.target.value)}/>
+                            </div>
 
-  
-    <div id = "HomePageWrapper">
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label" style={{float: 'left'}}>DRŽAVA</label>
+                                <input type="text" className="form-control"
+                                       disabled={true}
+                                       id="username" value={user.address?.country}
+                                       onChange={(e) => setInstitutionEmail(e.target.value)}/>
+                            </div>
 
-      <Navbar></Navbar>
-        <div className="row" id = "loginRow"  style={{paddingTop: "50px"}}>
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label" style={{float: 'left'}}>E-MAIL
+                                    INSTITUCIJE</label>
+                                <input type="text" className="form-control"
+                                       id="username" value={user.institution_email}
+                                       onChange={(e) => setInstitutionEmail(e.target.value)}/>
+                            </div>
+                        </div>
 
-          <div className="col-12 mx-auto " >
-            <h2 className = "text-start">Vaš profil</h2>
-            <p className = "text-start">
-            {
-              incorrectName ? ("Ime i prezime ") :
-                  incorrectOIB ? ("OIB je neispravan") :
-                      incorrectEmailFormat ? ("Email institucije nije ispravan") :
-                          saveFailed ? (errorMessage) : "Promijenite svoje podatke"
-            } </p>
-            <form onSubmit={handleSave} className ="pt-3">
+                        {/*------------------------INSTITUTION EMAIL-------------------------*/}
 
-              <div style={{display: "grid", gridTemplateColumns: "1fr 1fr", gridColumnGap: "20px"}}>
+                        <button type="submit" className="btn btn-primary col-12 py-2 mt-3">Spremi promjene</button>
+                        <button type="submit" style={{fontWeight: "bold"}} className="btn  btn-danger mt-2 col-12 py-2"
+                                onClick={(e) => handleSignOut()}>Odjavi se
+                        </button>
 
-              {/*----------------------------FIRST NAME-----------------------------*/}
-              <div className="mb-3">
-                <label htmlFor="username" className="form-label" style={{float: 'left'}}>IME</label>
-                <input type="text" className="form-control"
-                       id="username" value={firstName}
-                       onChange={(e) =>
-                           setFirstName(e.target.value.replace(/[^a-zA-ZščćžđöüäŠČĆŽĐÖÜÄ\\s]/, ''))}/>
-              </div>
-
-              {/*----------------------------LAST NAME-----------------------------*/}
-              <div className="mb-3">
-                <label htmlFor="username" className="form-label" style={{float: 'left'}}>PREZIME</label>
-                <input type="text" className="form-control"
-                       id="username" value={lastName}
-                       onChange={(e) =>
-                           setLastName(e.target.value.replace(/[^a-zA-ZščćžđöüäŠČĆŽĐÖÜÄ\\s]/, ''))}/>
-              </div>
-
-              {/*-------------------------------OIB--------------------------------*/}
-              <div className="mb-3">
-                <label htmlFor="username" className="form-label" style={{float: 'left'}}>OIB</label>
-                <input type="text" className="form-control"
-                       id="username" value={OIB}
-                       onChange={(e) =>
-                           setOIB(e.target.value.replace(/\D/g, ''))}/>
-              </div>
-
-              {/*------------------------INSTITUTION EMAIL-------------------------*/}
-              <div className="mb-3">
-                <label htmlFor="password" className="form-label" style={{float: 'left'}}>E-MAIL INSTITUCIJE</label>
-                <input type="text" className="form-control"
-                       id="username" value={institutionEmail}
-                       onChange={(e) => setInstitutionEmail(e.target.value)}/>
-              </div>
-             
-            
-              </div>
-              <button type="submit" className="btn btn-primary col-12 py-2 mt-3">Spremi promjene</button>
-              <button type="submit" style={{fontWeight: "bold"}} className="btn  btn-danger mt-2 col-12 py-2"
-                      onClick={(e) => handleSignOut()}>Odjavi se</button>
-
-            </form>
-          </div>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default ProfilePage;
