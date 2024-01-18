a2d5caeb2130444f07a51315d33d9901b1629fef
Dorian Matić <dorian.matic@fer.hr>
2024-01-18 18:32:35 +0100

    css fixes

--- IzvorniKod/ozdravi-frontend/src/components/NovaIspricnica.js
+++ IzvorniKod/ozdravi-frontend/src/components/NovaIspricnica.js
@@ -12,50 +12,32 @@ const NovaIspricnica = (props) => {
         props.closeIspricnica()
     }
 
-
     function handleSubmit(e) {
         e.preventDefault();
 
+        props.closeIspricnica()
     }
 
     return (
-        <div id = "addPatientsWrapper addIspricnica" className = "shadow-lg">
-
-
-            <div id = "addPatientsInner addIspricnica">
-
-
-                <h5 className = "pt-3 px-4 mt-2 mb-3 " style={{textAlign: "left"}}>Dodaj uputu{/* <button className='btn btn-tertiary mt-1' style={{float: 'right'}}>Povijest </button>  */} <img style={{ height: "23px", float: "right" }} onClick = {closeModal} src={CloseIcon}></img>  </h5>
-
-
-                <hr className = "mb-1 mt-4" style={{opacity: "20%"}}></hr>
-
-
-
-
-
-
-
-
+        <div id="addPatientsWrapper addIspricnica" className="shadow-lg">
+            <div id="addPatientsInner">
+                <h5 className="pt-3 px-4 mt-2 mb-3 " style={{textAlign: "left"}}>Stvori ispričnicu
+                    <img style={{height: "23px", float: "right"}} onClick={closeModal} src={CloseIcon}></img></h5>
+                <hr className="mb-1 mt-4" style={{opacity: "20%"}}></hr>
                 <div className='px-4 pt-0'>
-                    <form >
-
-                        <div style={{display: "grid", gridTemplateColumns: "1fr", gridColumnGap: "20px"}} class ="mt-4">
-
-
-
+                    <form>
+                        <div style={{display: "grid", gridTemplateColumns: "1fr", gridColumnGap: "20px"}} class="mt-4">
                             <div className="mb-3">
-                                <label htmlFor="username" className="form-label" style={{float: 'left'}}>Opis ispricnice </label>
-                                <textarea rows = "7" type="date" className="form-control" id="username"
+                                <label htmlFor="username" className="form-label" style={{float: 'left'}}>Opis
+                                    ispricnice </label>
+                                <textarea rows="7" type="date" className="form-control" id="username"
                                           placeholder={"Ukratko upišite razlog izostanka..."}/>
                             </div>
-
-
                         </div>
 
-
-                        <button className="btn btn-primary col-12 col-md-2 py-2 mb-4" style={{float:"right"}}
-                                onClick={handleSubmit}>Spremi </button>
+                        <button className="btn btn-primary col-12 col-md-2 py-2 mb-4" style={{float: "right"}}
+                                onClick={handleSubmit}>Spremi
+                        </button>
                     </form>
                 </div>
             </div>
